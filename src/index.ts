a7d34be9347cdbb10eef663d7e641b2b2483c67a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-11 11:33:15 +0800

    完善

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import fs from 'fs';
 import path from 'path';
 
 export
+function merge(obj1: any, obj2: any) {
+  const isObject = (object: any) =>
+    Object.prototype.toString.call(object) === '[object Object]';
+  if (isObject(obj1) && isObject(obj2)) {
+    const result = { ...obj1 };
+    Object.entries(obj2).forEach(([key, value]) =>
+      result[key] = merge(result[key], value)
+    );
+    return result;
+  }
+  return obj2;
+}
+
+export
 function checkDir(dirPath: string) {
   if (!fs.existsSync(dirPath))
     fs.mkdirSync(dirPath, { recursive: true });
@@ -19,6 +33,13 @@ function loadJson(filePath: string) {
   return { };
 }
 
+export
+function getAllSubDirPath(dirPath: string) {
+  return fs.readdirSync(checkDir(dirPath))
+    .map((file) => path.join(dirPath, file))
+    .filter((file) => fs.statSync(file).isDirectory());
+}
+
 export default
 function openfp(browserPath: string) {
   checkDir(browserPath);
@@ -26,4 +47,5 @@ function openfp(browserPath: string) {
   const pluginsPath = checkDir(path.join(browserPath, 'plugins'));
   const fingerprintPath = path.join(browserPath, 'fingerprint.json');
   const launchOptionsPath = path.join(browserPath, 'launchOptions.json');
+  const pluginsDirs = getAllSubDirPath(pluginsPath).join(',');
 }
