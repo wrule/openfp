b3a3da04e29f452dc49afc068e00bc51feb468d0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-11 11:26:00 +0800

    openfp

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import fs from 'fs';
 
 export
-function hello() {
-  console.log('hello world!');
+function checkDir(dirPath: string) {
+  if (!fs.existsSync(dirPath))
+    fs.mkdirSync(dirPath, { recursive: true });
+  return dirPath;
+}
+
+export default
+function openfp(browserPath: string) {
+
 }
