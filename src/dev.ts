05e6c25cff06cfad2c93675898f1d103eb277168
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-12 12:09:36 +0800

    采用绝对路径

--- src/dev.ts
+++ src/dev.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+import path from 'path';
 import openfp from '.';
 
 function dev() {
-  openfp(process.argv[2]);
+  openfp(path.resolve(process.cwd(), process.argv[2]));
 }
 
 dev();
